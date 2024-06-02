aa74bfac054e0c58550162a2eaaa0bb25c396004
MridulaMassey <24003590@massey.ac.nz>
2024-06-02 18:30:52 +1200

    Update appTest.py

--- appTest.py
+++ appTest.py
@@ -202,7 +202,11 @@ def statewise_prediction():
         
         # Predict the winner for the 2024 election using the best model
         predicted_winner_2024 = predict_winner_by_state(best_model, state_avg_wins)
-        st.write(f"Predicted winner for 2024 election by the best model ({best_model_name}): {predicted_winner_2024}")
+       # st.write(f"Predicted winner for 2024 election by the best model ({best_model_name}): {predicted_winner_2024}")
+        st.write(f"The best model is {best_model_name}")
+        predictions_df = pd.DataFrame(list(predicted_winner_2024.items()), columns=['State', 'Predicted Winner'])
+         # Display predictions as a table
+         st.table(predictions_df)
 
 def nationwide_prediction():
     st.title("Nationwide Prediction")
